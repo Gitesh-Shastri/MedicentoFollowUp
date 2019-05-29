3cd0c891b2fc76b6ce9659222e26adca754cd40a
nidhiDtu <nid1998dec@gmail.com>
2019-05-29 14:17:43 +0530

    Navigation drawer UI

--- app/src/main/java/com/medicento/medicentofollowup/MainActivity.java
+++ app/src/main/java/com/medicento/medicentofollowup/MainActivity.java
@@ -1,7 +1,11 @@
 package com.medicento.medicentofollowup;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
