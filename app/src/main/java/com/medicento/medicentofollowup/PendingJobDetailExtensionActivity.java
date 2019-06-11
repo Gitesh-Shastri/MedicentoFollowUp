e51996624f73d9e80013fb35fd304499c15c1d72
nidhiDtu <nid1998dec@gmail.com>
2019-06-11 12:58:28 +0530

    new work

---
+++ app/src/main/java/com/medicento/medicentofollowup/PendingJobDetailExtensionActivity.java
@@ -0,0 +1,13 @@
+package com.medicento.medicentofollowup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PendingJobDetailExtensionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pending_job_detail_extension);
+    }
+}
