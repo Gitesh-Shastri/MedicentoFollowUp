601729e3991afc811781ab923589509ad03ce1ea
nidhiDtu <nid1998dec@gmail.com>
2019-06-28 17:56:48 +0530

    feedbackactivity updated

--- app/src/main/java/com/medicento/medicentofollowup/FeedbackActivity.java
+++ app/src/main/java/com/medicento/medicentofollowup/FeedbackActivity.java
@@ -3,7 +3,9 @@ package com.medicento.medicentofollowup;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -13,14 +15,32 @@ import java.util.List;
 public class FeedbackActivity extends AppCompatActivity {
 
     private TextView nameTextView,
-            addressTextView;
+            addressTextView,
+            issueswithDistributorTV,
+            lateDeliveryTV,
+            latedeliveryExtendedTV,
+            availabilityOfMedTV,
+            availabilityOfMedExtendedTV,
+            issueswithDeliveryBoyTV,
+            othersTV,
+            reOnBoardedTV;
+
+    private Button submitButton;
+
+    private EditText distributorName,other1,other2;
 
     private LinearLayout issueWithDistributor,
             lateDelivery,
             availabilityOfMedicine,
-            issuesWithDeliveryBoy;
+            issuesWithDeliveryBoy,
+            lateDeliveryExtended,
+            availabilityOfMedicineExtended,
+            issuesWithDeliveryBoyExtended,
+            others,
+            othersLayout,
+            reOnBoarded;
 
-    private CheckBox yes1,no1,yes2,no2,yes3,no3,yes4,no4,yes5,yes6,yes7,no5,no6,no7;
+    private CheckBox yes1,no1,yes2,no2,yes3,no3,yes4,no4,yes5,yes6,yes7,no5,no6,no7,idk;
     private Feedbacks feedbacks = new Feedbacks();
     private Problems problems = new Problems();
     private List<String> listOfMedicineNotAvailable = new ArrayList<>();
@@ -33,8 +53,25 @@ public class FeedbackActivity extends AppCompatActivity {
 
         nameTextView = findViewById(R.id.nameTextView);
         addressTextView = findViewById(R.id.addressTextView);
+        issueswithDistributorTV = findViewById(R.id.textHint2);
+        lateDeliveryTV = findViewById(R.id.textHint3);
+        latedeliveryExtendedTV = findViewById(R.id.textHint6);
+        availabilityOfMedTV = findViewById(R.id.textHint4);
+        availabilityOfMedExtendedTV = findViewById(R.id.textHint7);
+        issueswithDeliveryBoyTV = findViewById(R.id.textHint5);
+        othersTV = findViewById(R.id.textHint8);
+        reOnBoardedTV = findViewById(R.id.textHint9);
+
         issueWithDistributor = findViewById(R.id.issueswithdistributor);
         issuesWithDeliveryBoy = findViewById(R.id.issueswithdeliveryboy);
+        lateDelivery = findViewById(R.id.latedelivery);
+        availabilityOfMedicine = findViewById(R.id.availabilityofmedicine);
+        lateDeliveryExtended = findViewById(R.id.latedeliveryextended);
+        availabilityOfMedicineExtended = findViewById(R.id.availabilityofmedicineextended);
+        issuesWithDeliveryBoyExtended = findViewById(R.id.issueswithdeliveryboyextended);
+        others = findViewById(R.id.others);
+        othersLayout = findViewById(R.id.otherslayout);
+        reOnBoarded = findViewById(R.id.reonboarded);
 
         yes1 = findViewById(R.id.yes1);
         yes2 = findViewById(R.id.yes2);
@@ -44,6 +81,19 @@ public class FeedbackActivity extends AppCompatActivity {
         no2 = findViewById(R.id.no2);
         no3 = findViewById(R.id.no3);
         no4 = findViewById(R.id.no4);
+        yes5 = findViewById(R.id.yes5);
+        yes6 = findViewById(R.id.yes6);
+        yes7 = findViewById(R.id.yes7);
+        no5 = findViewById(R.id.no5);
+        no6 = findViewById(R.id.no6);
+        no7 = findViewById(R.id.no7);
+        idk = findViewById(R.id.idk);
+
+        distributorName = findViewById(R.id.distributorname);
+        other1 = findViewById(R.id.other1);
+        other2 = findViewById(R.id.other2);
+
+        submitButton = findViewById(R.id.submitbutton);
 
         yes1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -64,6 +114,24 @@ public class FeedbackActivity extends AppCompatActivity {
             }
         });
         yes4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onCheckboxClicked(view.getId());
+            }
+        });
+        yes5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onCheckboxClicked(view.getId());
+            }
+        });
+        yes6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onCheckboxClicked(view.getId());
+            }
+        });
+        yes7.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 onCheckboxClicked(view.getId());
@@ -94,34 +162,109 @@ public class FeedbackActivity extends AppCompatActivity {
                 onCheckboxClicked(view.getId());
             }
         });
+        no5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onCheckboxClicked(view.getId());
+            }
+        });
+        no6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onCheckboxClicked(view.getId());
+            }
+        });
+        no7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onCheckboxClicked(view.getId());
+            }
+        });
     }
 
     public void onCheckboxClicked(int id){
         switch (id){
             case R.id.yes1:
-                no1.setSelected(false);
+                no1.setChecked(false);
                 break;
+                
             case R.id.yes2:
-                no2.setSelected(false);
+                latedeliveryExtendedTV.setVisibility(View.VISIBLE);
+                lateDeliveryExtended.setVisibility(View.VISIBLE);
+                no2.setChecked(false);
                 break;
+                
             case R.id.yes3:
-                no3.setSelected(false);
+                availabilityOfMedExtendedTV.setVisibility(View.VISIBLE);
+                availabilityOfMedicineExtended.setVisibility(View.VISIBLE);
+                no3.setChecked(false);
                 break;
+                
             case R.id.yes4:
-                no4.setSelected(false);
+                issuesWithDeliveryBoyExtended.setVisibility(View.VISIBLE);
+                no4.setChecked(false);
                 break;
+                
+            case R.id.yes5:
+                no5.setChecked(false);
+                break;
+                
+            case R.id.yes6:
+                othersLayout.setVisibility(View.VISIBLE);
+                break;
+                
+            case R.id.yes7:
+                no7.setChecked(false);
+                idk.setChecked(false);
+                break;
+                
             case R.id.no1:
-                yes1.setSelected(false);
+                yes1.setChecked(false);
                 break;
+                
             case R.id.no2:
-                yes2.setSelected(false);
+                latedeliveryExtendedTV.setVisibility(View.GONE);
+                lateDeliveryExtended.setVisibility(View.GONE);
+                yes2.setChecked(false);
                 break;
+                
             case R.id.no3:
-                yes3.setSelected(false);
+                availabilityOfMedExtendedTV.setVisibility(View.GONE);
+                availabilityOfMedicineExtended.setVisibility(View.GONE);
+                yes3.setChecked(false);
                 break;
+                
             case R.id.no4:
-                yes4.setSelected(false);
+                issuesWithDeliveryBoyExtended.setVisibility(View.GONE);
+                yes4.setChecked(false);
+                break;
+                
+            case R.id.no5:
+                yes5.setChecked(false);
+                break;
+                
+            case R.id.no6:
+                yes6.setChecked(false);
+                break;
+                
+            case R.id.no7:
+                idk.setChecked(false);
+                yes7.setChecked(false);
+                break;
+
+            case R.id.submitbutton:
+                
+                submit();
+                break;
+
+            case R.id.idk:
+                no7.setChecked(false);
+                yes7.setChecked(false);
                 break;
         }
+    }
+
+    private void submit() {
+
     }
 }
